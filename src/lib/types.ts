ab74bb3ce159411b30b431f5ec3f5adba628a3db
ToddLiao <toddliao.dev@gmail.com>
2024-01-26 00:59:11 +0800

    feat: add zod, fix date error

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,13 +1,48 @@
+import { z } from 'zod';
+
 interface Posts {
 	name: string;
 	url: string;
 	tags: string[];
 }
 
-export type Env =  {
+
+
+const notionQueryResponseSchema = z.object({
+	results: z.array(
+		z.object({
+			url: z.string(),
+			properties: z.object({
+				Name: z.object({
+					title: z.array(
+						z.object({
+							plain_text: z.string(),
+						})
+					),
+				}),
+				URL: z.object({
+					url: z.string(),
+				}),
+				Tags: z.object({
+					multi_select: z.array(
+						z.object({
+							name: z.string(),
+						})
+					),
+				}),
+			}),
+		})
+	),
+});
+
+type NotionQueryResponse = z.infer<typeof notionQueryResponseSchema>
+
+
+// Because of hono `Bindings` type, so we need to use `type` instead of `interface`
+type Env = {
 	NOTION_TOKEN: string;
 	NOTION_DATABASE_ID: string;
 	DISCORD_WEBHOOK_URL: string;
-}
+};
 
-export { Posts };
+export { Posts, NotionQueryResponse, Env, notionQueryResponseSchema };
